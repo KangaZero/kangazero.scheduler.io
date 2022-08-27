6d71ff5483dbf297feba2712b9898b1a647d79ee
Samuel <samuelyongw@gmail.com>
2022-08-27 19:05:39 +1000

    additional css classes, html with extra buttons for new functions

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
 
 var timeEl = document.querySelectorAll('#time');
 var currentTimeEl = $('#currentDay');
-
+var refreshBtnEl = $('#refreshBtn');
 
 //displays current time on page
 currentTimeEl.text(moment().format('dddd, MMMM Do'));
 
-
+//Activity 3 Week 5 //refreshes page to clear text
+refreshBtnEl.on('click', function () {
+    location.reload();
+  });
+  
 // function updatePerHour(){
 //     var currentHour = moment().hours();
 //     timeEl.forEach(function (){
