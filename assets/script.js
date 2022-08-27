dc9c46b30cd81bea833404a2ba30b89b21c7e80c
Samuel <samuelyongw@gmail.com>
2022-08-27 19:45:33 +1000

    Responsive bootstrap classes added (still working progress)

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,16 @@ currentTimeEl.text(moment().format('dddd, MMMM Do'));
 refreshBtnEl.on('click', function () {
     location.reload();
   });
-  
-// function updatePerHour(){
-//     var currentHour = moment().hours();
-//     timeEl.forEach(function (){
-//         var surroundingHour =
-//     })
 
-// }
+//Activity 3 Week 5 //Dark-light mode toogle switch //Edit this!
+  themeButtonEl.on('click', function () {
+    if (isDark) {
+      $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+      isDark = !isDark;
+    } else {
+      $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
+      isDark = !isDark;
+    }
+  });
+  
+  
