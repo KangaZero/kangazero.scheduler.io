ba0b3ec4444f049bccb2e39f2ba8c2f8ad7dc35f
Samuel <samuelyongw@gmail.com>
2022-08-28 20:15:07 +1000

    Timeblock highlight function added (hopefully working)

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@
 var timeEl = document.querySelectorAll('#time');
 var currentTimeEl = $('#currentDay');
 var refreshBtnEl = $('#refreshBtn');
+var themeBtnEl = $('.switch');
+
 
 //displays current time on page
 currentTimeEl.text(moment().format('dddd, MMMM Do'));
@@ -11,15 +13,36 @@ refreshBtnEl.on('click', function () {
     location.reload();
   });
 
+//test 
+function highlightHour (){
+    var timeNow = moment().hours();
+
+    timeEl.forEach(function(){
+        var hourIndicator = parseInt(timeEl.attr('id').split('-')[1]);
+
+        if (hourIndicator == timeNow){
+            timeEl.addClass('present');
+        } else if (hourIndicator < currentHour){
+            timeEl.removeClass('present');
+            timeEl.addClass('past');
+        } else {
+            timeEl.removeClass('present');
+            timeEl.removeClass('past');
+            timeEl.addClass('future');
+        }
+    });
+}
+//  var isDark = true;
 //Activity 3 Week 5 //Dark-light mode toogle switch //Edit this!
-  themeButtonEl.on('click', function () {
-    if (isDark) {
-      $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
-      isDark = !isDark;
-    } else {
-      $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
-      isDark = !isDark;
-    }
-  });
   
-  
+//   themeBtnEl.on('click', function () {
+//     if (isDark) {
+//       $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+//       isDark = !isDark;
+//     } else {
+//       $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
+//       isDark = !isDark;
+//     }
+//   });
+
+
